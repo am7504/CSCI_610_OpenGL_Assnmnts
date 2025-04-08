3c9c920dcc1e00bb40713c1e1379d1c02a4ed65d
Anjanm7504 <am7504@rit.edu>
2025-04-07 23:59:33 -0400

    testing why it doesnt work

--- transMain.js
+++ transMain.js
@@ -18,8 +18,8 @@ function createShapes() {
 
     myTeapot = new Teapot();
     myTeapot.VAO = bindVAO (myTeapot);
-    // myCube1 = new Cube(2);
-    // myCube1.VAO = bindVAO(myCube1);
+    myCube1 = new Cube(2);
+    myCube1.VAO = bindVAO(myCube1);
 
 }
 
@@ -32,14 +32,16 @@ function setUpCamera() {
     // set up your projection
     // defualt is orthographic projection
     let projMatrix = glMatrix.mat4.create();
-    glMatrix.mat4.ortho(projMatrix, -5, 5, -5, 5, 1.0, 300.0);
+    // glMatrix.mat4.ortho(projMatrix, -5, 5, -5, 5, 1.0, 300.0);
     
-    // glMatrix.mat4.perspective(projMatrix, radians(60), 1.0, 1.0, 300.0);
+    glMatrix.mat4.perspective(projMatrix, radians(60), 1.0, 1.0, 300.0);
+    gl.uniformMatrix4fv (program.uProjT, false, projMatrix);
+
     
     // set up your view
     // defaut is at (0,0,-5) looking at the origin
     let viewMatrix = glMatrix.mat4.create();
-    glMatrix.mat4.lookAt(viewMatrix, [5, 5, 5], [0, 0, 0], [0, 1, 0]);
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0]);
     gl.uniformMatrix4fv (program.uViewT, false, viewMatrix);
 }
 
@@ -57,17 +59,17 @@ function drawShapes() {
   let modelMatrixCube = glMatrix.mat4.create();
 
   // Rotate teapot around Y-axis
-  glMatrix.mat4.rotateY(modelMatrixTeapot, modelMatrixTeapot, radians(45.0));
+  glMatrix.mat4.rotateY(modelMatrixTeapot, modelMatrixTeapot, radians(90.0));
   gl.uniformMatrix4fv(program.uModelT, false, modelMatrixTeapot);
   gl.bindVertexArray(myTeapot.VAO);
   gl.drawElements(gl.TRIANGLES, myTeapot.indices.length, gl.UNSIGNED_SHORT, 0);
 
   // Translate cube
-  // glMatrix.mat4.translate(modelMatrixCube, modelMatrixCube, [0.0, -1.0, 0.0]);
-  // glMatrix.mat4.scale(modelMatrixCube, modelMatrixCube, [3.0, 0.5, 1.0]);
-  // gl.uniformMatrix4fv(program.uModelT, false, modelMatrixCube);
-  // gl.bindVertexArray(myCube1.VAO);
-  // gl.drawElements(gl.TRIANGLES, myCube1.indices.length, gl.UNSIGNED_SHORT, 0);
+  // glMatrix.mat4.scale(modelMatrixCube, modelMatrixCube, [3.0, 0.5, 0.0]);
+  glMatrix.mat4.translate(modelMatrixCube, modelMatrixCube, [-4.0, -3.0, 1.0]);
+  gl.uniformMatrix4fv(program.uModelT, false, modelMatrixCube);
+  gl.bindVertexArray(myCube1.VAO);
+  gl.drawElements(gl.TRIANGLES, myCube1.indices.length, gl.UNSIGNED_SHORT, 0);
 }
 
 ///////////////////////////////////////////////////////////////////
