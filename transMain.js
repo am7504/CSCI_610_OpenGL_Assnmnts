7728b6de6c98c477b4c0ebdb1e591431669e52bf
Anjanm7504 <am7504@rit.edu>
2025-04-07 23:57:10 -0400

    testing different camera

--- transMain.js
+++ transMain.js
@@ -32,9 +32,9 @@ function setUpCamera() {
     // set up your projection
     // defualt is orthographic projection
     let projMatrix = glMatrix.mat4.create();
-    // glMatrix.mat4.ortho(projMatrix, -5, 5, -5, 5, 1.0, 300.0);
+    glMatrix.mat4.ortho(projMatrix, -5, 5, -5, 5, 1.0, 300.0);
     
-    glMatrix.mat4.perspective(projMatrix, radians(60), 1.0, 1.0, 300.0);
+    // glMatrix.mat4.perspective(projMatrix, radians(60), 1.0, 1.0, 300.0);
     
     // set up your view
     // defaut is at (0,0,-5) looking at the origin
