a35b2b0e511219017be1c6586329da0ea0c07aee
Anjanm7504 <am7504@rit.edu>
2025-04-07 23:31:23 -0400

    adding self

--- cgIShape.js
+++ cgIShape.js
@@ -46,6 +46,8 @@ class Cube extends cgIShape {
         const squareWidth = 1 / (2 ** subdivisions); // Size of each small square
         const half = 0.5; // Cube goes from -0.5 to 0.5
 
+        const self = this;
+
         const createFace = (normal, uAxis, vAxis, offset) => {
             for (let i = 0; i < 2 ** subdivisions; i++) {
                 for (let j = 0; j < 2 ** subdivisions; j++) {
@@ -64,11 +66,11 @@ class Cube extends cgIShape {
 
                     // Two triangles per square
                     if (reverse) {
-                        this.addTriangle(p1.x, p1.y, p1.z, p3.x, p3.y, p3.z, p2.x, p2.y, p2.z);
-                        this.addTriangle(p2.x, p2.y, p2.z, p3.x, p3.y, p3.z, p4.x, p4.y, p4.z);
+                        self.addTriangle(p1.x, p1.y, p1.z, p3.x, p3.y, p3.z, p2.x, p2.y, p2.z);
+                        self.addTriangle(p2.x, p2.y, p2.z, p3.x, p3.y, p3.z, p4.x, p4.y, p4.z);
                     } else {
-                        this.addTriangle(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z, p3.x, p3.y, p3.z);
-                        this.addTriangle(p2.x, p2.y, p2.z, p4.x, p4.y, p4.z, p3.x, p3.y, p3.z);
+                        self.addTriangle(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z, p3.x, p3.y, p3.z);
+                        self.addTriangle(p2.x, p2.y, p2.z, p4.x, p4.y, p4.z, p3.x, p3.y, p3.z);
                     }
                 }
             }
