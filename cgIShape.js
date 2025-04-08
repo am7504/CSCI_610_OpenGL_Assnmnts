6623738640f38bc57966d416c445f00c3a989afa
Anjanm7504 <am7504@rit.edu>
2025-04-07 23:22:23 -0400

    changing addTriangle to super

--- cgIShape.js
+++ cgIShape.js
@@ -64,11 +64,11 @@ class Cube extends cgIShape {
 
                     // Two triangles per square
                     if (reverse) {
-                        this.addTriangle(p1.x, p1.y, p1.z, p3.x, p3.y, p3.z, p2.x, p2.y, p2.z);
-                        this.addTriangle(p2.x, p2.y, p2.z, p3.x, p3.y, p3.z, p4.x, p4.y, p4.z);
+                        super.addTriangle(p1.x, p1.y, p1.z, p3.x, p3.y, p3.z, p2.x, p2.y, p2.z);
+                        super.addTriangle(p2.x, p2.y, p2.z, p3.x, p3.y, p3.z, p4.x, p4.y, p4.z);
                     } else {
-                        this.addTriangle(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z, p3.x, p3.y, p3.z);
-                        this.addTriangle(p2.x, p2.y, p2.z, p4.x, p4.y, p4.z, p3.x, p3.y, p3.z);
+                        super.addTriangle(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z, p3.x, p3.y, p3.z);
+                        super.addTriangle(p2.x, p2.y, p2.z, p4.x, p4.y, p4.z, p3.x, p3.y, p3.z);
                     }
                 }
             }
